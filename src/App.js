5af4889a0862f20fd0ff97f0447c832e1bf01f9e
Cole Pedersen <colepedersen@CP.local>
2018-11-25 09:14:45 -0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+// COMPONENTS
 import Contact from "./components/Contact";
 import Header from "./components/Header";
-
+// CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -9,21 +11,23 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manger" />
-        <Contact
-          name="Bradley Beal"
-          email="bbgun@gmail.com"
-          phone="444-444-4444"
-        />
-        <Contact
-          name="JK Rowling"
-          email="jkRowl@gmail.com"
-          phone="555-555-5555"
-        />
-        <Contact
-          name="Tyrion Lanister"
-          email="kingofthenorth@gmail.com"
-          phone="777-777-7777"
-        />
+        <div className="container">
+          <Contact
+            name="Bradley Beal"
+            email="bbgun@gmail.com"
+            phone="444-444-4444"
+          />
+          <Contact
+            name="JK Rowling"
+            email="jkRowl@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Tyrion Lanister"
+            email="kingofthenorth@gmail.com"
+            phone="777-777-7777"
+          />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
 // rfc Functional Component
 import React from "react";
 import PropTypes from "prop-types";
+// CSS
 import "./css/header.css";
 
 const Header = props => {
   // DESTRUCTURING
   const { branding } = props;
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home2
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
