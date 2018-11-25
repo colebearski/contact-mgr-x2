d1e7a37143b608fcf5be90d008b9c7e123b51cad
Cole Pedersen <colepedersen@CP.local>
2018-11-25 08:43:20 -0700

    destruct rfc and rcc

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@
 import React from "react";
 
 const Header = props => {
+  // DESTRUCTURING
+  const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 };
