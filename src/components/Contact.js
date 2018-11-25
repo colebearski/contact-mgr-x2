a427258f65a7052d92b7c983eca3ea9e0dbc7799
Cole Pedersen <colepedersen@CP.local>
2018-11-25 10:27:02 -0700

    events and custom methods

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,12 +4,25 @@ import PropTypes from "prop-types";
 import "./css/contact.css";
 
 class Contact extends Component {
+  state = {};
+
+  // Custom Methods should use arrow functions for 'this'
+  // eliminates the need to bind or run constructors
+  onShowClick = e => {
+    console.log(e.target);
+  };
+
   render() {
     // DESTRUCTURING
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {/* {this.onshowClick} BECAUSE IT'S A METHOD INSIDE THIS CLASS */}
+          {/* you can bind additional parameters to the function  */}
+          {/* this.onShowClick.bind(this, name, etc..) */}
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
