6703f5a81f9e54d023c40f9fb7cabdd2c4bdb2a1
Cole Pedersen <colepedersen@CP.local>
2018-11-25 08:39:55 -0700

    rcc props w/o dest

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manger" />
-        <Contact />
+        <Contact
+          name="Bradley Beal"
+          email="bbgun@gmail.com"
+          phone="444-444-4444"
+        />
+        <Contact
+          name="JK Rowling"
+          email="jkRowl@gmail.com"
+          phone="555-555-5555"
+        />
+        <Contact
+          name="Tyrion Lanister"
+          email="kingofthenorth@gmail.com"
+          phone="777-777-7777"
+        />
       </div>
     );
   }
