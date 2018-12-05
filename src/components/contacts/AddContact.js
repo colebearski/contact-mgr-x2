16789aa84543747406b7863a5e970ec9f247fa18
Cole Pedersen <cole.pedersen@gmail.com>
2018-12-04 20:00:45 -0700

    post request to fake rest api

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Consumer } from "../../Context";
 import TextInputGroup from "../layout/TextInputGroup";
-import uuid from "uuid";
+// import uuid from "uuid";
+import axios from "axios";
 
 class AddContact extends Component {
   state = {
@@ -37,13 +38,16 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
+      // id: uuid(),
       name,
       email,
       phone
     };
 
-    dispatch({ type: "ADD_CONTACT", payload: newContact });
+    // Post request to fake rest api
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newContact)
+      .then(res => dispatch({ type: "ADD_CONTACT", payload: res.data }));
 
     // Clear state
     this.setState({
